c4cd78d6377151e28dddf8e9cc66177e710e2339
IFNET-FOR\felipe.pinheiro <felipe.pinheiro@ifactory.com.br>
2018-11-02 14:00:34 -0300

    Endpoint cliente/id disponivel

---
+++ src/main/java/com/ifactory/cursomc/Services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.ifactory.cursomc.Services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.spel.support.ReflectivePropertyAccessor.OptimalPropertyAccessor;
+import org.springframework.stereotype.Service;
+
+import com.ifactory.cursomc.Services.Exceptions.ObjectNotFoundException;
+import com.ifactory.cursomc.domain.Cliente;
+import com.ifactory.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(    "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); 
+	}
+
+}
