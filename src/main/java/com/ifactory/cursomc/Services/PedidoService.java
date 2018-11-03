cbc89f4f68fef5469c4623bb6682ce7584fa3b56
IFNET-FOR\felipe.pinheiro <felipe.pinheiro@ifactory.com.br>
2018-11-03 11:28:41 -0300

    endpoint/pedidos/id disponibilizado

---
+++ src/main/java/com/ifactory/cursomc/Services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.ifactory.cursomc.Services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ifactory.cursomc.Services.Exceptions.ObjectNotFoundException;
+import com.ifactory.cursomc.domain.Pedido;
+import com.ifactory.cursomc.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(    "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName())); 
+	}
+
+}
