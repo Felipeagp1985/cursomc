aafdbf09d7848ff1f3c7c22d6f87f3a89a88a9fd
IFNET-FOR\felipe.pinheiro <felipe.pinheiro@ifactory.com.br>
2018-11-05 16:49:10 -0200

    Atualizando categorias com PUT

--- src/main/java/com/ifactory/cursomc/Services/ClienteService.java
+++ src/main/java/com/ifactory/cursomc/Services/ClienteService.java
@@ -3,7 +3,6 @@ package com.ifactory.cursomc.Services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.expression.spel.support.ReflectivePropertyAccessor.OptimalPropertyAccessor;
 import org.springframework.stereotype.Service;
 
 import com.ifactory.cursomc.Services.Exceptions.ObjectNotFoundException;
@@ -16,9 +15,10 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
-	public Cliente buscar(Integer id) {
+	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(    "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); 
 	}
 
 }
+
--- src/main/java/com/ifactory/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ifactory/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,8 @@
 package com.ifactory.cursomc.resources;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,8 +22,8 @@ public class CategoriaResource {
 	CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 
 	}
@@ -37,6 +34,13 @@ public class CategoriaResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 
 }
