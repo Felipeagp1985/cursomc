014526c49ef598c550439fec9d7fa61d3a927f75
IFNET-FOR\felipe.pinheiro <felipe.pinheiro@ifactory.com.br>
2018-11-05 16:35:18 -0200

    Post para categoria

--- src/main/java/com/ifactory/cursomc/Services/CategoriaService.java
+++ src/main/java/com/ifactory/cursomc/Services/CategoriaService.java
@@ -3,7 +3,6 @@ package com.ifactory.cursomc.Services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.expression.spel.support.ReflectivePropertyAccessor.OptimalPropertyAccessor;
 import org.springframework.stereotype.Service;
 
 import com.ifactory.cursomc.Services.Exceptions.ObjectNotFoundException;
@@ -19,6 +18,11 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(    "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 
 }
