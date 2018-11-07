7477a4cd7bfb3b71c212ee9b7783d428e652ec80
IFNET-FOR\felipe.pinheiro <felipe.pinheiro@ifactory.com.br>
2018-11-07 19:57:19 -0200

    Email não repetido na atualização de cliente

--- src/main/java/com/ifactory/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/ifactory/cursomc/dto/ClienteDTO.java
@@ -7,8 +7,10 @@ import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.ifactory.cursomc.Services.validation.ClientUpdate;
 import com.ifactory.cursomc.domain.Cliente;
 
+@ClientUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
