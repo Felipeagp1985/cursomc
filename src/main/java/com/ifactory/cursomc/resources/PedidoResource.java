d1d5da085ce1097d3d61f82ca14c73b17e6c8e88
IFNET-FOR\felipe.pinheiro <felipe.pinheiro@ifactory.com.br>
2018-11-07 22:33:21 -0200

    Inserindo Pedido

--- src/main/java/com/ifactory/cursomc/resources/PedidoResource.java
+++ src/main/java/com/ifactory/cursomc/resources/PedidoResource.java
@@ -1,13 +1,20 @@
 package com.ifactory.cursomc.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ifactory.cursomc.Services.PedidoService;
+import com.ifactory.cursomc.domain.Categoria;
 import com.ifactory.cursomc.domain.Pedido;
 
 @RestController
@@ -22,6 +29,14 @@ public class PedidoResource {
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
